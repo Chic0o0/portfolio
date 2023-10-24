ec50e5c5f147bc91e3eebd5e34aa3cbf6d1357e1
unknown <chicokds@gmail.com>
2023-10-24 14:47:14 +0200

    Finished slides

--- src/components/Box/Box.js
+++ src/components/Box/Box.js
@@ -1,12 +1,33 @@
 import React from 'react';
 import './Box.css';
 
+//willDo add more props and styling
+
 const Box = ({title, image, text}) => (
   <div className="Box">
-    <h1>{title}</h1>
+    <h2>{title}</h2>
     <img src={image}/>
     <p>{text}</p>
   </div>
 );
 
 export default Box;
+
+// export default function Box({title, image, text}){
+//   if (typeof text === "undefined") {
+//     return(
+//       <div className="Box">
+//         <h2>{title}</h2>
+//         <img src={image}/>
+//       </div>
+//     )
+//   } else {
+//     return(
+//       <div className="Box">
+//         <h2>{title}</h2>
+//         <img src={image}/>
+//         <p>{text}</p>
+//       </div>
+//     )
+//   }
+// }
--- src/components/Slides/Slides.js
+++ src/components/Slides/Slides.js
@@ -3,6 +3,7 @@ import './Slides.css';
 import { Slide } from 'react-slideshow-image';
 import 'react-slideshow-image/dist/styles.css';
 import Box from './../Box/Box.js'
+import Icon1 from './../../media/mail.svg'
 
 const buttonStyle = {
     background: 'none',
@@ -11,19 +12,137 @@ const buttonStyle = {
 };
 
 const properties = {
-    prevArrow: <button style={{ ...buttonStyle }}><svg viewBox="0 0 1024 1024" class="icon" xmlns="http://www.w3.org/2000/svg" fill="#000000" transform="rotate(0)"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><path fill="#000000" d="M529.408 149.376a29.12 29.12 0 0141.728 0 30.592 30.592 0 010 42.688L259.264 511.936l311.872 319.936a30.592 30.592 0 01-.512 43.264 29.12 29.12 0 01-41.216-.512L197.76 534.272a32 32 0 010-44.672l331.648-340.224zm256 0a29.12 29.12 0 0141.728 0 30.592 30.592 0 010 42.688L515.264 511.936l311.872 319.936a30.592 30.592 0 01-.512 43.264 29.12 29.12 0 01-41.216-.512L453.76 534.272a32 32 0 010-44.672l331.648-340.224z"></path></g></svg></button>,
-    nextArrow: <button style={{ ...buttonStyle }}><svg viewBox="0 0 1024 1024" class="icon" xmlns="http://www.w3.org/2000/svg" fill="#000000" transform="rotate(180)"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><path fill="#000000" d="M529.408 149.376a29.12 29.12 0 0141.728 0 30.592 30.592 0 010 42.688L259.264 511.936l311.872 319.936a30.592 30.592 0 01-.512 43.264 29.12 29.12 0 01-41.216-.512L197.76 534.272a32 32 0 010-44.672l331.648-340.224zm256 0a29.12 29.12 0 0141.728 0 30.592 30.592 0 010 42.688L515.264 511.936l311.872 319.936a30.592 30.592 0 01-.512 43.264 29.12 29.12 0 01-41.216-.512L453.76 534.272a32 32 0 010-44.672l331.648-340.224z"></path></g></svg></button>
+    prevArrow: <button style={{ ...buttonStyle }}><svg viewBox="0 0 1024 1024" className="icon" xmlns="http://www.w3.org/2000/svg" fill="#000000" transform="rotate(0)"><g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g><g id="SVGRepo_iconCarrier"><path fill="#000000" d="M529.408 149.376a29.12 29.12 0 0141.728 0 30.592 30.592 0 010 42.688L259.264 511.936l311.872 319.936a30.592 30.592 0 01-.512 43.264 29.12 29.12 0 01-41.216-.512L197.76 534.272a32 32 0 010-44.672l331.648-340.224zm256 0a29.12 29.12 0 0141.728 0 30.592 30.592 0 010 42.688L515.264 511.936l311.872 319.936a30.592 30.592 0 01-.512 43.264 29.12 29.12 0 01-41.216-.512L453.76 534.272a32 32 0 010-44.672l331.648-340.224z"></path></g></svg></button>,
+    nextArrow: <button style={{ ...buttonStyle }}><svg viewBox="0 0 1024 1024" className="icon" xmlns="http://www.w3.org/2000/svg" fill="#000000" transform="rotate(180)"><g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g><g id="SVGRepo_iconCarrier"><path fill="#000000" d="M529.408 149.376a29.12 29.12 0 0141.728 0 30.592 30.592 0 010 42.688L259.264 511.936l311.872 319.936a30.592 30.592 0 01-.512 43.264 29.12 29.12 0 01-41.216-.512L197.76 534.272a32 32 0 010-44.672l331.648-340.224zm256 0a29.12 29.12 0 0141.728 0 30.592 30.592 0 010 42.688L515.264 511.936l311.872 319.936a30.592 30.592 0 01-.512 43.264 29.12 29.12 0 01-41.216-.512L453.76 534.272a32 32 0 010-44.672l331.648-340.224z"></path></g></svg></button>
 }
 
 const Slides = () => (
   <div className="Slides">
-    <Slide autoplay={false} {...properties}>
+    <Slide autoplay={false} canSwipe={false} {...properties}>
         <div className="EachSlide">
             <h1>Title1</h1>
             <div className='Boxes'>
                 <Box
                     title={"A"}
-                    img={"./../../media/image.jpg"}
+                    image={Icon1}
+                />
+                <Box
+                    title={"A"}
+                    image={Icon1}
+                />
+                <Box
+                    title={"A"}
+                    image={Icon1}
+                    text={"Whatever"}
+                />
+                <Box
+                    title={"A"}
+                    image={Icon1}
+                    text={"Whatever"}
+                />
+                <Box
+                    title={"A"}
+                    image={Icon1}
+                    text={"Whatever"}
+                />
+                <Box
+                    title={"A"}
+                    image={Icon1}
+                    text={"Whatever"}
+                />
+                <Box
+                    title={"A"}
+                    image={Icon1}
+                    text={"Whatever"}
+                />
+                <Box
+                    title={"A"}
+                    image={Icon1}
+                    text={"Whatever"}
+                />
+                <Box
+                    title={"A"}
+                    image={Icon1}
+                    text={"Whatever"}
+                />
+                <Box
+                    title={"A"}
+                    image={Icon1}
+                    text={"Whatever"}
+                />
+                <Box
+                    title={"A"}
+                    image={Icon1}
+                    text={"Whatever"}
+                />
+                <Box
+                    title={"A"}
+                    image={Icon1}
+                    text={"Whatever"}
+                />
+                <Box
+                    title={"A"}
+                    image={Icon1}
+                    text={"Whatever"}
+                />
+                <Box
+                    title={"A"}
+                    image={Icon1}
+                    text={"Whatever"}
+                />
+                <Box
+                    title={"A"}
+                    image={Icon1}
+                    text={"Whatever"}
+                />
+                <Box
+                    title={"A"}
+                    image={Icon1}
+                    text={"Whatever"}
+                />
+                <Box
+                    title={"A"}
+                    image={Icon1}
+                    text={"Whatever"}
+                />
+                <Box
+                    title={"A"}
+                    image={Icon1}
+                    text={"Whatever"}
+                />
+                <Box
+                    title={"A"}
+                    image={Icon1}
+                    text={"Whatever"}
+                />
+                <Box
+                    title={"A"}
+                    image={Icon1}
+                    text={"Whatever"}
+                />
+                <Box
+                    title={"A"}
+                    image={Icon1}
+                    text={"Whatever"}
+                />
+                <Box
+                    title={"A"}
+                    image={Icon1}
+                    text={"Whatever"}
+                />
+                <Box
+                    title={"A"}
+                    image={Icon1}
+                    text={"Whatever"}
+                />
+                <Box
+                    title={"A"}
+                    image={Icon1}
+                    text={"Whatever"}
+                />
+                <Box
+                    title={"Final"}
+                    image={Icon1}
                     text={"Whatever"}
                 />
             </div>
